7b2c19e4ae4e796097b6f9991f809d029a39576a
Roy Fiegenschue <royjohn76@gmail.com>
2015-03-27 15:04:46 +0000

    stripe changes

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   Stripe.setPublishableKey($('meta[name="stripe-key"]').attr('content'));
-  //Watch for a for submission:
+  // Watch for a form submission:
   $("#form-submit-btn").click(function(event) {
     event.preventDefault();
     $('input[type=submit]').prop('disabled', true);
@@ -9,37 +9,34 @@ $(document).ready(function() {
         cvcNum = $('#card_code').val(),
         expMonth = $('#card_month').val(),
         expYear = $('#card_year').val();
-    
-    if(!error) {
-      //Get the Stripe token
+
+    if (!error) {
+      // Get the Stripe token:
       Stripe.createToken({
         number: ccNum,
         cvc: cvcNum,
         exp_month: expMonth,
         exp_year: expYear
       }, stripeResponseHandler);
-      
     }
     return false;
   }); // form submission
   
   function stripeResponseHandler(status, response) {
-    //get a reference to the form:
+    // Get a reference to the form:
     var f = $("#new_user");
-    
-    //Get the token from the response:
+
+    // Get the token from the response:
     var token = response.id;
-    
-    //Add the token to the form:
+
+    // Add the token to the form:
     f.append('<input type="hidden" name="user[stripe_card_token]" value="' + token + '" />');
-    
-    //submit the form:
-    
-    f.get(0).submit();
-    
+
+    // Submit the form:
+    f.get(0).submit(); 
   }
-  
 });
-  }                              
+
+                                
                               
      
