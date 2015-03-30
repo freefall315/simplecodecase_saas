3cb5eb395a7e79e988ad76d384ee291b5dfd6f98
Roy Fiegenschue <royjohn76@gmail.com>
2015-03-30 00:03:58 +0000

    added profile new action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,3 +1,9 @@
 class ProfilesController < ApplicationController
+  def new 
+    # form where a user can file out thier own profile.
+    @user = User.find( params[:user_id] )
+    @profile = @user.build_profile 
+    
+  end
   
 end
